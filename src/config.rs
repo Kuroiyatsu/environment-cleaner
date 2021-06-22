1f39153b0ff78bfe4be9105c85b4a692696feac8
Antonio Johnson <johnsson.ad@live.com>
2021-06-21 19:59:40 -0500

    logic for settings struct

--- src/config.rs
+++ src/config.rs
@@ -1,32 +1,44 @@
 use std::fs::{self, File};
+pub struct Config {
+    config_path: &'static str,
+    config_file: &'static str,
+}
 
-/// Ensures configuration file is created, and loads file.
-pub fn setup_config() {
-    let config_path = "C:\\ProgramData\\Environment Cleaner\\";
-    let config_file = "config.yaml";
-
-    if !has_config_file(config_path, config_file) {
-        create_file(config_path, config_file);
+impl Config {
+    pub fn new() -> Config {
+        Config {
+            config_path: "C:\\ProgramData\\Environment Cleaner\\",
+            config_file: "config.yaml",
+        }
     }
-}
 
-/// Checks if configuration file exists.
-fn has_config_file(config_path: &str, config_file: &str) -> bool{
-    let full_file_path = [config_path, config_file].join("");//format!("{}{}", config_path, config_file);
-    
-    std::path::Path::new(&full_file_path[..]).exists()
-}
+    /// Ensures configuration file is created, and loads file.
+    pub fn setup(&self) {
+        if !self.has_config_file() {
+            self.create_file();
+        }
+    }
 
-/// Creates configuration file;
-fn create_file(config_path: &str, config_file: &str) {
-    println!("No configuration file found.");
+    /// Checks if configuration file exists.
+    fn has_config_file(&self) -> bool {
+        let full_file_path = [self.config_path, self.config_file].join(""); //format!("{}{}", config_path, config_file);
 
-    let full_file_path = [config_path, config_file].join("");
+        std::path::Path::new(&full_file_path[..]).exists()
+    }
 
-    println!("Creating file {}", full_file_path);
+    /// Creates configuration file;
+    fn create_file(&self) {
+        println!("No configuration file found.");
 
-    fs::create_dir_all(config_path).unwrap();
-    File::create(full_file_path).unwrap();
+        let full_file_path = [self.config_path, self.config_file].join("");
 
-    println!("Configuration file created!");
+        println!("Creating file {}", full_file_path);
+
+        fs::create_dir_all(self.config_path).unwrap();
+        File::create(full_file_path).unwrap();
+
+        println!("Configuration file created!");
+    }
+
+    fn initialize() {}
 }
